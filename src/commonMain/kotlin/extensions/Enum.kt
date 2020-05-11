e06a71a18e21b46633e84926b135773ad65cdbde
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-05-11 15:20:36 +0900

    Add Enum extension functions (#2)
    Add Enum extension functions

---
+++ src/commonMain/kotlin/extensions/Enum.kt
@@ -0,0 +1,18 @@
+package extensions
+
+interface EnumExtension<E : Enum<E>>
+
+inline fun <reified E : Enum<E>> EnumExtension<E>.valueOf(name: String, default: E) =
+    enumValues<E>().firstOrNull { it.name == name } ?: default
+
+inline fun <reified E : Enum<E>> EnumExtension<E>.valueOf(ordinal: Int, default: E) =
+    enumValues<E>().firstOrNull { it.ordinal == ordinal } ?: default
+
+inline fun <reified E : Enum<E>> EnumExtension<E>.valueOfWithCondition(condition: (E) -> Boolean) =
+    enumValues<E>().firstOrNull(condition)
+
+inline fun <reified E : Enum<E>> EnumExtension<E>.valueOfWithCondition(condition: (E) -> Boolean, default: E) =
+    enumValues<E>().firstOrNull(condition) ?: default
+
+inline fun <reified E : Enum<E>> EnumExtension<E>.valueOfIgnoreCase(name: String) =
+    enumValues<E>().firstOrNull { it.name.toLowerCase() == name.toLowerCase() }
