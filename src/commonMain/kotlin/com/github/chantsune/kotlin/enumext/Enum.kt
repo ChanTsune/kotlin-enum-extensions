483217004997827d15fefe59beba10669b3948b9
ChanTsuen <41658782+ChanTsune@users.noreply.github.com>
2020-08-30 15:10:37 +0900

    add kdoc comment

--- src/commonMain/kotlin/com/github/chantsune/kotlin/enumext/Enum.kt
+++ src/commonMain/kotlin/com/github/chantsune/kotlin/enumext/Enum.kt
@@ -1,26 +1,44 @@
 package com.github.chantsune.kotlin.enumext
 
+/**
+ * Returns an enum entry matching the given [predicate], or `null` if entry was not found.
+ */
 public inline fun <reified E : Enum<E>> enumValueOfOrNull(predicate: (E) -> Boolean): E? {
     return enumValues<E>().firstOrNull(predicate)
 }
 
+/**
+ * Returns an enum entry with specified [name], or `null` if entry was not found.
+ */
 public inline fun <reified E : Enum<E>> enumValueOfOrNull(name: String, ignoreCase: Boolean = false): E? {
     return if (ignoreCase) enumValueOfOrNull<E> { it.name.toLowerCase() == name.toLowerCase() }
     else enumValueOfOrNull<E> { it.name == name }
 }
 
+/**
+ * Returns an enum entry with specified [ordinal], or `null` if entry was not found.
+ */
 public inline fun <reified E : Enum<E>> enumValueOfOrNull(ordinal: Int): E? {
     return enumValueOfOrNull<E> { it.ordinal == ordinal }
 }
 
+/**
+ * Returns an enum entry with specified [name], or [default] if entry was not found.
+ */
 public inline fun <reified E : Enum<E>> enumValueOf(name: String, default: E, ignoreCase: Boolean): E {
     return enumValueOfOrNull<E>(name, ignoreCase) ?: default
 }
 
+/**
+ * Returns an enum entry with specified [ordinal], or [default] if entry was not found.
+ */
 public inline fun <reified E : Enum<E>> enumValueOf(ordinal: Int, default: E): E {
     return enumValueOfOrNull<E>(ordinal) ?: default
 }
 
+/**
+ * Returns an enum entry matching the given [predicate], or [default] if entry was not found.
+ */
 public inline fun <reified E : Enum<E>> enumValueOf(default: E, predicate: (E) -> Boolean): E {
     return enumValueOfOrNull<E>(predicate) ?: default
 }
--- src/commonMain/kotlin/com/github/chantsune/kotlin/enumext/EnumExtension.kt
+++ src/commonMain/kotlin/com/github/chantsune/kotlin/enumext/EnumExtension.kt
@@ -1,19 +1,34 @@
 package com.github.chantsune.kotlin.enumext
 
+/**
+ * Inherited by the companion object of Enum class to provide an extended function.
+ */
 public interface EnumExtension<E : Enum<E>>
 
+/**
+ * Returns an enum entry with specified [name], or `null` if entry was not found.
+ */
 public inline fun <reified E : Enum<E>> EnumExtension<E>.valueOfOrNull(name: String, ignoreCase: Boolean = false): E? {
     return enumValueOfOrNull<E>(name, ignoreCase)
 }
 
+/**
+ * Returns an enum entry with specified [ordinal], or `null` if entry was not found.
+ */
 public inline fun <reified E : Enum<E>> EnumExtension<E>.valueOfOrNull(ordinal: Int): E? {
     return enumValueOfOrNull<E>(ordinal)
 }
 
+/**
+ * Returns an enum entry matching the given [predicate], or `null` if entry was not found.
+ */
 public inline fun <reified E : Enum<E>> EnumExtension<E>.valueOfOrNull(predicate: (E) -> Boolean): E? {
     return enumValueOfOrNull<E>(predicate)
 }
 
+/**
+ * Returns an enum entry with specified [name], or [default] if entry was not found.
+ */
 public inline fun <reified E : Enum<E>> EnumExtension<E>.valueOf(
     name: String,
     default: E,
@@ -22,10 +37,16 @@ public inline fun <reified E : Enum<E>> EnumExtension<E>.valueOf(
     return enumValueOf(name, default, ignoreCase)
 }
 
+/**
+ * Returns an enum entry with specified [ordinal], or [default] if entry was not found.
+ */
 public inline fun <reified E : Enum<E>> EnumExtension<E>.valueOf(ordinal: Int, default: E): E {
     return enumValueOf(ordinal, default)
 }
 
+/**
+ * Returns an enum entry matching the given [predicate], or [default] if entry was not found.
+ */
 public inline fun <reified E : Enum<E>> EnumExtension<E>.valueOf(default: E, predicate: (E) -> Boolean): E {
     return enumValueOf(default, predicate)
 }
