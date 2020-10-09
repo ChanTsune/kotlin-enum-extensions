cf462a868663081334049ca8e2714c518cf2500b
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-10-09 19:03:57 +0900

    Update kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.publish.maven.MavenPom
 
 plugins {
-    kotlin("multiplatform") version "1.4.0"
+    kotlin("multiplatform") version "1.4.10"
 
     id("org.jetbrains.dokka") version "1.4.0"
 
