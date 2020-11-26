963f62e1cdb446be57a471be34b41e4d7dcb3fcc
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-11-26 17:09:53 +0900

    Update Dokka "1.4.10" -> "1.4.10.2"

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.publish.maven.MavenPom
 plugins {
     kotlin("multiplatform") version "1.4.10"
 
-    id("org.jetbrains.dokka") version "1.4.10"
+    id("org.jetbrains.dokka") version "1.4.10.2"
 
     id("maven-publish")
 }
