9eac9ca3038d155177a63389e08b9dbc65b7f9f8
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-06-22 14:50:09 +0000

    Deploying to gh-pages from @ ChanTsune/kotlin-enum-extensions@cebdc614eea01325881169c44625edb33d66c1b7 🚀

--- scripts/pages.js
+++
@@ -1,1 +0,0 @@
-var pages = [{"name":"interface EnumExtension<E : Enum<E>>","description":"com.github.chantsune.kotlin.enumext.EnumExtension","location":"kotlin-enum-extensions/com.github.chantsune.kotlin.enumext/-enum-extension/index.html","searchKeys":["EnumExtension","interface EnumExtension<E : Enum<E>>"]}]
